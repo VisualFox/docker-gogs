86e2dfa4fd179d1736a86b7d3d84ccabbe1d3cd8
Philippe Blanc <p@visualfox.me>
2016-02-09 02:01:45 +0100

    add sqlite support

--- res/app/docker/build.sh
+++ res/app/docker/build.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 
 # Install build deps
-apk -U --no-progress add go@community
+apk -U --no-progress add go@community gcc musl-dev
 
 # Set temp environment vars
 export GOPATH=/tmp/go
@@ -12,7 +12,8 @@ export PATH=${PATH}:${GOPATH}/bin
 # build Gogs
 go get -u github.com/gogits/gogs
 cd ${GOPATH}/src/github.com/gogits/gogs
-go build
+go get -v -tags "sqlite"
+go build -tags "sqlite"
 
 # Create git user for Gogs
 adduser -H -D -g 'Gogs Git User' git -h /data/git -s /bin/bash && passwd -u git
@@ -22,11 +23,11 @@ echo "export GOGS_CUSTOM=${GOGS_CUSTOM}" >> /etc/profile
 mv $GOPATH/src/github.com/gogits/gogs /app/
 
 # folder and permission
-ln -s /data/gogs/data /app/data && chown -h git:git /app/gogs/data
-ln -s /data/gogs/log /app/log && chown -h git:git /app/gogs/log
+ln -s /data/gogs/data /app/gogs/data && chown -h git:git /app/gogs/data
+ln -s /data/gogs/log /app/gogs/log && chown -h git:git /app/gogs/log
 
 # Remove build deps
-apk --no-progress del go
+apk --no-progress del go gcc musl-dev
 
 # Cleanup GOPATH
 rm -r $GOPATH
